caf3dd5db818b01c5c034e064f636345250ebd3f
WalkersGB10 <2flamingfishes@gmail.com>
2025-03-04 09:52:35 +0000

    Update Letter list.py

--- Letter list.py
+++ Letter list.py
@@ -1,1 +1,15 @@
-words = ["tacocat", "racecar", "oliver", ""]
+words = ["tacocat", "racecar", "oliver", "bradford", "high", "mill", "pneumonoultramicroscopicsilicovolcanoconiosis", "hippopotomonstrosesquippedaliophobia", "oyster", "wellington", "word", "can", "they", "be", "numbers", "fifteen"]
+
+letter = input("What is your letter?").lower()[0]
+
+accepted = []
+for i in words:
+    if i[0] == letter:
+        accepted.append(i)
+for i in accepted:
+    print(i)
+if len(accepted) == 0:
+    print("No words in the list start with", letter)
+
+else:
+    print("All begin with", letter)
