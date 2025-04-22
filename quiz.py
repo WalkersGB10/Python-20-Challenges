3c002546bd776b6eef94777fe6a1b373e27c9049
WalkersGB10 <2flamingfishes@gmail.com>
2025-04-22 14:36:18 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,9 @@
+q = ["what is jamesNC?", "what does arlo do?", "how is Oliver Walker?"]
+a = ["cool", "smell", "great"]
+
+score = 0
+for i in range (0, len(q)):
+    ans = input(q[i])
+    if ans == a[i]:
+        score +=1
+print(score, "/ 3")
